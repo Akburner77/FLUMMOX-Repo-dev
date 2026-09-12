32e1c7ba4470f911b832894521e98670027924e7
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-12 16:19:34 +0530

    Add repository URL to cloudstream configuration

--- BingeCloud/build.gradle.kts
+++ BingeCloud/build.gradle.kts
@@ -27,6 +27,7 @@ kotlin {
 }
 
 cloudstream {
+    repositoryUrl = "https://raw.githubusercontent.com/FlummoxGamer/FLUMMOX-Repo/builds"
     description = "BingeCloud - Movies & TV Series"
     authors = listOf("FlummoxGamer")
     status = 1
