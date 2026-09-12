1779c751378e03f094bc5ea65ad38f75c1de7b98
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-12 14:31:02 +0530

    Update Gradle and Kotlin plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.android.tools.build:gradle:8.13.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
     }
 }
