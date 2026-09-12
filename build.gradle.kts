e4c63067170dc39794addc45c1d2461cec857811
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-12 18:55:28 +0530

    Refactor build.gradle.kts structure and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,20 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("com.lagradost.cloudstream3.gradle")
-}
-
-android {
-    namespace = "com.flummox.bingecloud"
-    compileSdk = 35
-    defaultConfig {
-        minSdk = 21
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
     }
-    buildFeatures {
-        buildConfig = true
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.13.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
     }
-}
-
-cloudstream {
-    description = "BingeCloud - VegaMovies provider"
-    authors = listOf("FlummoxGamer")
-    status = 1
-    tvTypes = listOf("Movie", "TvSeries")
-    language = "hi"
-}
-
-dependencies {
-    val cloudstream by configurations
-    cloudstream("com.lagradost:cloudstream3:pre-release")
-    implementation("com.github.Blatzar:NiceHttp:0.4.11")
-    implementation("org.jsoup:jsoup:1.18.3")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.13")
 }
