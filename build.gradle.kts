7eeeb4c4a58778debf1b9214c4404498fcf228b0
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-12 14:02:47 +0530

    Remove Android library subproject configuration
    Removed subproject configuration for Android library and Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,26 +18,3 @@ allprojects {
         maven("https://jitpack.io")
     }
 }
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "org.jetbrains.kotlin.android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    extensions.configure<com.android.build.gradle.LibraryExtension> {
-        compileSdk = 34
-        defaultConfig {
-            minSdk = 21
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-        }
-    }
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
-    }
-}
