d27e8ce7e8a238859b73fc2c4146163d629f4cd4
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-12 13:50:19 +0530

    Upgrade Gradle plugin and configure subprojects
    Updated Android Gradle plugin version and added subproject configurations for Android library.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,9 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.android.tools.build:gradle:8.7.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
 
@@ -15,5 +16,28 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "org.jetbrains.kotlin.android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    extensions.configure<com.android.build.gradle.LibraryExtension> {
+        compileSdk = 34
+        defaultConfig {
+            minSdk = 21
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 }
