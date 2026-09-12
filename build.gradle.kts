35c2ddc1aa0863d15e1a667952ce0a200ed6808d
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-12 16:28:16 +0530

    Upgrade Gradle and Kotlin plugin versions
    Updated Gradle plugin versions and removed unused configurations.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 buildscript {
     repositories {
         google()
@@ -9,9 +5,9 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:8.13.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
     }
 }
 
@@ -20,39 +16,5 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-    }
-}
-
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.getByName("cloudstream").configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) =
-    extensions.getByName("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "FlummoxGamer/FLUMMOX-Repo")
-    }
-
-    android {
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(33)
-            targetSdk = 33
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
     }
 }
