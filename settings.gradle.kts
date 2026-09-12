87f68c900ac028b628ebe871ac7de7fc365156b0
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-12 18:51:07 +0530

    Configure plugin resolution strategy in settings.gradle.kts
    Added resolution strategy for plugin versions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,15 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://jitpack.io")
     }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "com.android.library" -> useModule("com.android.tools.build:gradle:8.13.0")
+                "org.jetbrains.kotlin.android" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+                "com.lagradost.cloudstream3.gradle" -> useModule("com.github.recloudstream:gradle:-SNAPSHOT")
+            }
+        }
+    }
 }
 
 rootProject.name = "FLUMMOX-Repo"
