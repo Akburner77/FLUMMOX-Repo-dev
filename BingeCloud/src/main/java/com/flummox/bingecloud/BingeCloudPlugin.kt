995c0a63f70428a235a8c030b1b1e74e6fb097ac
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-12 14:56:11 +0530

    Add BingeCloudPlugin class for Cloudstream integration

---
+++ BingeCloud/src/main/java/com/flummox/bingecloud/BingeCloudPlugin.kt
@@ -0,0 +1,12 @@
+package com.flummox.bingecloud
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class BingeCloudPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(BingeCloud())
+    }
+}
