c12634d1ba54d3b590926dc0b381037ccf5ae3fc
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-13 15:31:41 +0530

    Remove dev test comment from VegaMoviesProvider.kt

--- BingeCloud/src/main/java/com/flummox/bingecloud/VegaMoviesProvider.kt
+++ BingeCloud/src/main/java/com/flummox/bingecloud/VegaMoviesProvider.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-// dev test
+
 package com.flummox.bingecloud
 
 import com.lagradost.cloudstream3.*
