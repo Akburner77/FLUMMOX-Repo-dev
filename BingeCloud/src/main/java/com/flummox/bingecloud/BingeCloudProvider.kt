19e8f2bc63abec08a39de6fbc63d083be1bc70d2
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-15 22:54:12 +0530

    Remove unused import for ProgressiveResolver

--- BingeCloud/src/main/java/com/flummox/bingecloud/BingeCloudProvider.kt
+++ BingeCloud/src/main/java/com/flummox/bingecloud/BingeCloudProvider.kt
@@ -1,6 +1,5 @@
 package com.flummox.bingecloud
 
-import com.flummox.bingecore.ProgressiveResolver
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.utils.*
